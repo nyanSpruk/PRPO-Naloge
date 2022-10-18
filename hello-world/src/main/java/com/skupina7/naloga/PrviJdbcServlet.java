51c888d47f3cad845fdb3540ce8606f16dd4b0c1
bl4ko <go3969@student.uni-lj.si>
2022-10-18 11:16:14 +0200

    Removing spaces and reordering println statements

--- hello-world/src/main/java/com/skupina7/naloga/PrviJdbcServlet.java
+++ hello-world/src/main/java/com/skupina7/naloga/PrviJdbcServlet.java
@@ -5,27 +5,22 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.kumuluz.ee.configuration.utils.ConfigurationUtil;
-
 import java.io.IOException;
 
-@WebServlet("/servlet") // Kje servlet poslusa
+@WebServlet("/servlet") 
 public class PrviJdbcServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
         throws ServletException, IOException {
 
-            System.out.println("This is my first step to becoming a java champion!");
-
-            resp.getWriter().println("Hello fellow java champion!");
-
             final String service_name = ConfigurationUtil.getInstance().get("kumuluzee.name").get();
             final String service_version = ConfigurationUtil.getInstance().get("kumuluzee.version").get();
             final String environment_name = ConfigurationUtil.getInstance().get("kumuluzee.env.name").get();
 
-            resp.getWriter().println("service.name: " + service_name + "\n" + 
+            resp.getWriter().println("Hello fellow java champion!\n\n" +
+                                     "service.name: " + service_name + "\n" + 
                                      "service.version: " + service_version + "\n" +
                                      "service.environment.name: " + environment_name);
     }
